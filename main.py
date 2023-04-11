06b3c6cf3994856f976c71af62f0c840f70870a2
OliSew <93936682+OliSew@users.noreply.github.com>
2023-04-11 20:29:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+password = input("enter password:")
+user1 = ("I like cats")
+user2 = ("I like dogs")
+if password == user1:
+    print("logged as user1")
+    print("money 2345$")
+if password == user2:
+    print("logged as user2")
+    print("money 222$")
+else:
+    print("not logged")
